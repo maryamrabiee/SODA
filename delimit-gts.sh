e83870c5e94582a0d5003d9fb4b03230e6b34d4f
maryamrabiee <rabiei.maryam@gmail.com>
2019-11-05 10:57:21 -0800

    script added

---
+++ delimit-gts.sh
@@ -0,0 +1,12 @@
+gt=$1
+sp=$2
+out=$3
+outdir=$4
+jarfile=$5
+
+test $# -ne "3" && echo "Usage: $0 [Gene Tree File] [Species Tree File] [Output Name] [Out directory] [ASTRAL jar file]" && exit 1
+
+java -jar $jarfile  -i $gt -o $outdir/$out.tre -q $sp -p0 -t10 2> $outdir/$out.err 
+nw_reroot $outdir/$out.tre $(nw_labels -L $outdir/$out.tre | sort -n | head -1) | nw_topology - > $outdir/$out.tre.rt
+java -jar $jarfile  -i $gt -o $outdir/${out}2.tre -q $outdir/$out.tre.rt -p0 -t10 2> $outdir/${out}2.err
+python3 ./unrooted_species_delimitation.py $outdir/${out}2.tre $outdir/$out.cl 0.05 > /dev/null
